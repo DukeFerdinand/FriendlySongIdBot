399f4442de8c60f0ab5d4af0d3bdb2133da08f1b
Duke_Ferdinand <doug@dougflynn.dev>
2023-04-03 17:59:14 -0700

    Add simple greeting message to dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,1 +1,8 @@
-<h1 class="text-5xl text-white">Hello, twitch!</h1>
+<script lang="ts">
+	export let data;
+</script>
+
+<div class="flex">
+	<img src={data.idObject.picture} alt={data.idObject.preferred_username} />
+	<h1 class="text-5xl text-white">Hello, {data.idObject.preferred_username}!</h1>
+</div>
